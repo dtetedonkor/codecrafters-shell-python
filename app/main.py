db355b494210b58012ae5f5418cfbd600b857ab3
ethospathoslogos <nana1tete2@gmail.com>
2026-07-25 18:01:28 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import sys
+from pathlib import Path
 
+
+        
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sh_builtin = {"exit","type","echo"}
+    sh_builtin = {"exit","type","echo"} #builtin set for quick retreval 
     while(1):
         sys.stdout.write("$ ")
         command = input()
@@ -13,11 +16,11 @@ def main():
         command_list = command.split()
         arguments = ' '.join(command_list[1:])
 
-        if command_list[0] == "echo":
+        if command_list[0] == "echo": #echo builtin
             print(arguments)
             continue
 
-        if command_list[0] == "type":
+        if command_list[0] == "type": # type builtin handeling
             if arguments in sh_builtin:
                 print(f"{arguments} is a shell builtin")
             else:
