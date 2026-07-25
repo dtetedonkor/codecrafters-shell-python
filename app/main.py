7718dc4c4b8f48ac48d825cef0a2638aa318681a
ethospathoslogos <nana1tete2@gmail.com>
2026-07-24 21:34:12 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,14 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    command = sys.stdin.readline()
+    print(f"{command.rstrip()} : command not found")
+
     pass
+
+    
+
+
 
 
 if __name__ == "__main__":
