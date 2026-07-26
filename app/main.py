2f3ec2dd0e932a027b26e586570ef4d32761e42d
ethospathoslogos <nana1tete2@gmail.com>
2026-07-26 11:37:40 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,44 +2,68 @@ import sys
 import shutil
 import subprocess
 
+def type_builtin(command_list) -> None:
+    sh_builtin = {"exit","type","echo"} #builtin set for quick retreval
+    arguments = ' '.join(command_list[1:])
+    if command_list[0] == "type": # type builtin handeling
+                if arguments in sh_builtin:
+                    print(f"{arguments} is a shell builtin")
+                elif shutil.which(arguments):
+                    print(shutil.which(arguments))
+                else:
+                    print(f"{arguments}: not found")
 
-        
+def exit_bultin(command_list) -> bool:
+           if ' '.join(command_list) == "exit":
+                return True 
+           return False 
 
-def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sh_builtin = {"exit","type","echo"} #builtin set for quick retreval 
-    while(1):
+def echo_builtin(command_list) -> None:
+       
+       arguments = ' '.join(command_list[1:])
+       if command_list[0] == "echo": #echo builtin
+                  print(arguments)
+def builtin_check (command_list) -> bool:
+       sh_builtin = {"exit","type","echo"}
+       if command_list[0] in sh_builtin:
+              return True
+       return False
+
+def  run_program(command_list) -> None:
+         if shutil.which(command_list[0]):
+                   pl = subprocess.run(command_list,
+                                     capture_output=True,
+                                     text=True) 
+                   sys.stdout.write(pl.stdout) 
+         else:
+             print(f"{' '.join(command_list)}: command not found")   
+     
+def my_shell() -> list:
+    
+  
         sys.stdout.write("$ ")
         command = input()
 
-        if command == "exit":
-            break
-
         command_list = command.split()
-        arguments = ' '.join(command_list[1:])
 
-        if command_list[0] == "echo": #echo builtin
-            print(arguments)
-            continue
+        return command_list
+                       
+        
 
-        if command_list[0] == "type": # type builtin handeling
-            if arguments in sh_builtin:
-                print(f"{arguments} is a shell builtin")
-            elif shutil.which(arguments):
-                print(shutil.which(arguments))
-            else:
-                print(f"{arguments}: not found")
-            continue
+def main():
+    while(1):
+        sh = my_shell()
+        if exit_bultin(sh):
+              break
+        if builtin_check:
+            echo_builtin(sh)
+            type_builtin(sh)
+        else:
+              run_program(sh)
+        
+               
 
-        if shutil.which(command_list[0]):
-            pl = subprocess.run(command_list,
-                              capture_output=True,
-                              text=True) 
-            sys.stdout.write(pl.stdout)
-            continue
-            
-
-        print(f"{command.rstrip()}: command not found")
+        
         
         
 
